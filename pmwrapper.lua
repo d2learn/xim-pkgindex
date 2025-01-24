ec4d1e6efbaccd15e55fe1e750af0c177f26d216
sunrisepeak <speakshen@163.com>
2025-01-25 01:18:22 +0800

    pmwrapper: add manjaro support

--- pmwrapper.lua
+++ pmwrapper.lua
@@ -8,6 +8,7 @@ pmwrapper = {
     ["gcc"] = {
         ubuntu = {"apt", "gcc"},
         archlinux = {"pacman", "gcc"},
+        manjaro = {"pacman", "gcc"},
     },
     ["g++"] = { ref = "gcc" },
     ["java"] = { ref = "openjdk8" },
@@ -15,44 +16,55 @@ pmwrapper = {
     ["jdk8"] = { ref = "openjdk8" },
     ["mdbook"] = {
         archlinux = {"pacman", "mdbook"},
+        manjaro = {"pacman", "mdbook"},
     },
     ["make"] = {
         ubuntu = {"apt", "make"},
         archlinux = {"pacman", "make"},
+        manjaro = {"pacman", "make"},
     },
     ["nodejs"] = {
         archlinux = {"pacman", "nodejs"},
+        manjaro = {"pacman", "nodejs"},
     },
     ["nvm"] = {
         archlinux = {"aur", "nvm"},
+        manjaro = {"aur", "nvm"},
     },
     ["openjdk"] = { ref = "openjdk8" },
     ["openjdk8"] = {
         windows = {"winget", "AdoptOpenJDK.OpenJDK.8"},
         ubuntu = {"apt", "openjdk-8-jdk"},
         archlinux = {"pacman", "jdk8-openjdk"},
+        manjaro = {"pacman", "jdk8-openjdk"},
     },
     ["pnpm"] = {
         archlinux = {"pacman", "pnpm"},
+        manjaro = {"pacman", "pnpm"},
     },
     ["project-graph"] = {
         archlinux = {"aur", "project-graph-bin"},
+        manjaro = {"aur", "project-graph-bin"},
     },
     ["project-graph-nightly"] = {
         archlinux = {"aur", "project-graph-nightly-bin"},
+        manjaro = {"aur", "project-graph-nightly-bin"},
     },
     ["python"] = {
         windows = {"winget", "Python.Python.3.13"},
         ubuntu = {"apt", "python3"},
         archlinux = {"pacman", "python"},
+        manjaro = {"pacman", "python"},
     },
     ["rust"] = {
         archlinux = {"pacman", "rustup"},
+        manjaro = {"pacman", "rustup"},
     },
     ["vim"] = {
         windows = {"winget", "vim.vim"},
         ubuntu = {"apt", "vim"},
-        archlinux = {"pacman", "vim"}
+        archlinux = {"pacman", "vim"},
+        manjaro = {"pacman", "vim"},
     },
     ["visual-studio"] = { ref = "vs2022" },
     ["vs2022"] = {
@@ -61,10 +73,12 @@ pmwrapper = {
     ["vscode"] = {
         windows = {"winget", "Microsoft.VisualStudioCode"},
         archlinux = {"pacman", "code"},
+        manjaro = {"pacman", "code"},
     },
     ["webkit2gtk"] = {
         ubuntu = {"apt", "libwebkit2gtk-4.0-37"},
         archlinux = {"pacman", "webkit2gtk"},
+        manjaro = {"pacman", "webkit2gtk"},
     },
     ["wsl"] = {
         windows = {"winget", "Microsoft.WSL"},

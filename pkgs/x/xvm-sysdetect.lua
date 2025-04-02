50bd30ab1034ab20d234f1d4796d60886ac4f938
sunrisepeak <speakshen@163.com>
2025-04-02 22:30:11 +0800

    add xvm-sysdetect

---
+++ pkgs/x/xvm-sysdetect.lua
@@ -0,0 +1,80 @@
+package = {
+    -- base info
+    name = "xvm-sysdetect",
+    description = "XVM - System Package Detect Tools",
+
+    authors = "sunrisepeak",
+    license = "Apache-2.0",
+    repo = "https://github.com/d2learn/xim-pkgindex",
+
+    -- xim pkg info
+    type = "config",
+    status = "stable", -- dev, stable, deprecated
+    categories = {"xim", "xvm"},
+    keywords = {"xvm", "detect-tool"},
+
+    -- xvm: xlings version management
+    xvm_enable = true,
+
+    xpm = {
+        linux = {
+            ["latest"] = { },
+        },
+    },
+}
+
+local sys_paths = {
+    linux = {
+        "/usr/bin",
+        "/usr/local/bin",
+        "/bin",
+        "/sbin",
+        "/usr/sbin",
+        "/usr/local/sbin",
+    },
+}
+
+local pkgs = {
+    "python",
+    "gcc",
+    "g++",
+    "java",
+    "make",
+    "cmake",
+    "git",
+}
+
+function installed()
+    for _, pkg in ipairs(pkgs) do
+        if string.find(os.iorun("xvm list " .. pkg), "system", 1, true) == nil then
+            return false
+        end
+    end
+    return true
+end
+
+function install()
+    for _, pkg in ipairs(pkgs) do
+        for _, p in ipairs(sys_paths.linux) do
+            if os.isfile(path.join(p, pkg)) then
+                os.exec("xvm add " .. pkg .. " system" .. " --path " .. p)
+                break
+            end
+        end
+    end
+    return true
+end
+
+function config()
+    cprint("")
+    cprint("\t run [${yellow bright}xvm use ${green}name${clear} ${yellow bright}system${clear}] switch to system version")
+    cprint("")
+    return true
+end
+
+function uninstall()
+    for _, pkg in ipairs(pkgs) do
+        os.exec("xvm remove " .. pkg .. " system")
+    end
+    return true
+end

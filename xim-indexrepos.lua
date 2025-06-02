a517881bb5e348e2063a7a6d39e1bd85e6e66774
sunrisepeak <speakshen@163.com>
2025-06-03 01:24:03 +0800

    update sub-indexrepo mirror

--- xim-indexrepos.lua
+++ xim-indexrepos.lua
@@ -1,5 +1,11 @@
 xim_indexrepos = {
     -- ["namespace"] = "url",
-    ["xim"] = "https://github.com/d2learn/xim-pkgindex-template.git",
-    ["d2x"] = "https://github.com/d2learn/xim-pkgindex-d2x.git",
+    ["xim"] = {
+        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-template.git",
+        ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-template.git",
+    },
+    ["d2x"] = {
+        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-d2x.git",
+        ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-d2x.git",
+    },
 }

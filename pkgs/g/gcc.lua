42e5c74c5c8cc616361effa5e934c71bca79e54f
sunrisepeak <speakshen@163.com>
2025-06-09 12:32:20 +0800

    gcc: 15 -> binary package

--- pkgs/g/gcc.lua
+++ pkgs/g/gcc.lua
@@ -22,9 +22,8 @@ package = {
 
     xpm = {
         linux = {
-            deps = { "make", "gcc", "xz", "gzip", "bzip2" },
             ["latest"] = { ref = "15.1.0" },
-            ["15.1.0"] = { url = __gcc_url("15.1.0") },
+            ["15.1.0"] = "XLINGS_RES",
             ["14.2.0"] = { url = __gcc_url("14.2.0") },
             ["13.3.0"] = { url = __gcc_url("13.3.0") },
             ["12.4.0"] = { url = __gcc_url("12.4.0") },
@@ -39,43 +38,11 @@ import("xim.libxpkg.log")
 import("xim.libxpkg.xvm")
 
 function install()
-    local builddir = path.join(pkginfo.install_dir(), "xim_build")
-    local objdir = path.join(pkginfo.install_dir(), "xim_build", "objdir")
-    local prerequisites_dir = path.join(path.directory(pkginfo.install_dir()), "comm-prerequisites")
-
-    log.info("0.clean build cache...")
-    if not os.isdir(prerequisites_dir) then os.mkdir(prerequisites_dir) end
-    for _, dir in ipairs(os.dirs(path.join(prerequisites_dir, "**"))) do
-        -- if dir is empty, remove it
-        if os.emptydir(dir) then
-            os.tryrm(dir)
-        end
-    end
-
-    os.tryrm(builddir)
-    os.mkdir(builddir)
-    system.exec(string.format("tar xvf gcc-%s.tar.xz -C %s", pkginfo.version(), builddir))
-    os.cd(path.join(builddir, "gcc-" .. pkginfo.version()))
-
-    log.info("1.download prerequisites...")
-    -- readfile - contrib/download_prerequisites
-    local filecontent = io.readfile("contrib/download_prerequisites")
-    filecontent = filecontent:replace("--no-verbose", " ", { plain = true })
-    io.writefile("contrib/download_prerequisites", filecontent)
-    system.exec("contrib/download_prerequisites --directory=" .. prerequisites_dir)
-
-    log.info("2.build config...")
-    os.mkdir(objdir)
-    os.cd(objdir)
-    system.exec(string.format([[%s/gcc-%s/configure
-        --prefix=%s --enable-languages=c,c++ --disable-multilib
-    ]], builddir, pkginfo.version(), pkginfo.install_dir()))
-
-    log.info("3.build gcc...")
-    system.exec("time make -j32", { retry = 2 })
-
-    log.info("4.install gcc...")
-    system.exec("make install")
+    local gccdir = pkginfo.install_file()
+        :replace(".tar.gz", "")
+        :replace(".zip", "")
+    os.tryrm(pkginfo.install_dir())
+    os.mv(gccdir, pkginfo.install_dir())
     return true
 end
 

c2e562ca90ab7bfb10a7bb30c8a4bd03425cec1a
sunrisepeak <speakshen@163.com>
2025-07-22 21:54:43 +0800

    nodejs: add support for macos

--- pkgs/n/nodejs.lua
+++ pkgs/n/nodejs.lua
@@ -1,6 +1,8 @@
 function download_url(version)
     if is_host("windows") then
         return string.format("https://nodejs.org/dist/v%s/node-v%s-win-x64.zip", version, version)
+    elseif is_host("macosx") then
+        return string.format("https://nodejs.org/dist/v%s/node-v%s-darwin-arm64.tar.gz", version, version)
     else
         return string.format("https://nodejs.org/dist/v%s/node-v%s-linux-x64.tar.xz", version, version)
     end
@@ -24,22 +26,37 @@ package = {
 
     xpm = {
         windows = {
-            ["latest"] = { ref = "22.12.0" },
+            ["latest"] = { ref = "22.17.1" },
+            ["24.4.1"] = { url = download_url("24.4.1"), sha256 = nil, },
             ["23.6.0"] = { url = download_url("23.6.0"), sha256 = nil, },
+            ["22.17.1"] = { url = download_url("22.17.1"), sha256 = nil, },
             ["22.12.0"] = {
                 url = "https://nodejs.org/dist/v22.12.0/node-v22.12.0-win-x64.zip",
                 sha256 = "2b8f2256382f97ad51e29ff71f702961af466c4616393f767455501e6aece9b8",
             },
         },
         linux = {
-            ["latest"] = { ref = "22.14.0" },
+            ["latest"] = { ref = "22.17.1" },
+            ["24.4.1"] = { url = download_url("24.4.1"), sha256 = nil, },
             ["23.11.0"] = { url = download_url("23.11.0"), sha256 = nil, },
             ["23.6.0"] = { url = download_url("23.6.0"), sha256 = nil, },
+            ["22.17.1"] = { url = download_url("22.17.1"), sha256 = nil, },
             ["22.14.0"] = { url = download_url("22.14.0"), sha256 = nil, },
             ["22.12.0"] = {
                 url = "https://nodejs.org/dist/v22.12.0/node-v22.12.0-linux-x64.tar.xz",
                 sha256 = "22982235e1b71fa8850f82edd09cdae7e3f32df1764a9ec298c72d25ef2c164f",
             },
+            ["20.19.0"] = { url = download_url("20.19.0"), sha256 = nil, },
+            ["18.20.8"] = { url = download_url("18.20.8"), sha256 = nil, },
+        },
+        macosx = {
+            ["latest"] = { ref = "22.17.1" },
+            ["24.4.1"] = { url = download_url("24.4.1"), sha256 = nil, },
+            ["23.11.0"] = { url = download_url("23.11.0"), sha256 = nil, },
+            ["23.6.0"] = { url = download_url("23.6.0"), sha256 = nil, },
+            ["22.17.1"] = { url = download_url("22.17.1"), sha256 = nil, },
+            ["22.14.0"] = { url = download_url("22.14.0"), sha256 = nil, },
+            ["22.12.0"] = { url = download_url("22.12.0"), sha256 = nil, },
             ["20.19.0"] = { url = download_url("20.19.0"), sha256 = nil, },
             ["18.20.8"] = { url = download_url("18.20.8"), sha256 = nil, },
         },
@@ -53,10 +70,6 @@ local node_dir_template = {
     linux = "node-v%s-linux-x64",
     windows = "node-v%s-win-x64",
 }
-
-function installed()
-    return os.iorun("xvm list nodejs")
-end
 
 function install()
     os.tryrm(pkginfo.install_dir())

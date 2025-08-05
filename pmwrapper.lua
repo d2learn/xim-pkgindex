a36f4273c71e2e0367ecb6609564f058bc5ee546
MoYingJi <moyingjiaw@outlook.com>
2025-08-06 00:29:51 +0800

    [pmwrapper] add some archlinux pkgs (#29)
    * [pmwrapper] (M)wechat (A)qq to bwrap
    
    - 将 wechat 从 portable 版本更改为 bwrap 版本
    - 新增 qq 的 bwrap 版本
    
    * [pmwrapper] code-oss/vscode/vscodium
    
    * [pmwrapper] ninja - archlinux
    
    * [pmwrapper] dotnet dotnet-preview
    
    * [pmwrapper] delete manjaro

--- pmwrapper.lua
+++ pmwrapper.lua
@@ -10,6 +10,15 @@ pmwrapper = {
         ubuntu = {"apt", "cmake"},
         archlinux = {"pacman", "cmake"},
     },
+    ["code-oss"] = {
+        archlinux = {"pacman", "code"},
+    },
+    ["dotnet"] = {
+        archlinux = {"pacman", "dotnet-sdk"},
+    },
+    ["dotnet-preview"] = {
+        archlinux = {"aur", "dotnet-sdk-preview-bin"},
+    },
     ["dotnet-9"] = {
         windows = {"winget", "Microsoft.DotNet.SDK.9"},
     },
@@ -17,12 +26,10 @@ pmwrapper = {
         windows = {"winget", "fzf"},
         ubuntu = {"apt", "fzf"},
         archlinux = {"pacman", "fzf"},
-        manjaro = {"pacman", "fzf"},
     },
     ["gcc"] = {
         ubuntu = {"apt", "gcc"},
         archlinux = {"pacman", "gcc"},
-        manjaro = {"pacman", "gcc"},
     },
     ["gzip"] = {
         ubuntu = {"apt", "gzip"},
@@ -34,49 +41,45 @@ pmwrapper = {
     ["jdk8"] = { ref = "openjdk8" },
     ["mdbook"] = {
         archlinux = {"pacman", "mdbook"},
-        manjaro = {"pacman", "mdbook"},
     },
     ["make"] = {
         ubuntu = {"apt", "make"},
         archlinux = {"pacman", "make"},
-        manjaro = {"pacman", "make"},
+    },
+    ["ninja"] = {
+        archlinux = {"pacman", "ninja"},
     },
     ["nodejs"] = {
         archlinux = {"pacman", "nodejs"},
-        manjaro = {"pacman", "nodejs"},
     },
     ["nvm"] = {
         archlinux = {"aur", "nvm"},
-        manjaro = {"aur", "nvm"},
     },
     ["openjdk"] = { ref = "openjdk8" },
     ["openjdk8"] = {
         windows = {"winget", "AdoptOpenJDK.OpenJDK.8"},
         ubuntu = {"apt", "openjdk-8-jdk"},
         archlinux = {"pacman", "jdk8-openjdk"},
-        manjaro = {"pacman", "jdk8-openjdk"},
     },
     ["pnpm"] = {
         archlinux = {"pacman", "pnpm"},
-        manjaro = {"pacman", "pnpm"},
     },
     ["project-graph"] = {
         archlinux = {"aur", "project-graph-bin"},
-        manjaro = {"aur", "project-graph-bin"},
     },
     ["project-graph-nightly"] = {
         archlinux = {"aur", "project-graph-nightly-bin"},
-        manjaro = {"aur", "project-graph-nightly-bin"},
     },
     ["python"] = {
         windows = {"winget", "Python.Python.3.13"},
         ubuntu = {"apt", "python3"},
         archlinux = {"pacman", "python"},
-        manjaro = {"pacman", "python"},
+    },
+    ["qq"] = {
+        archlinux = {"aur", "linuxqq-nt-bwrap"},
     },
     ["rust"] = {
         archlinux = {"pacman", "rustup"},
-        manjaro = {"pacman", "rustup"},
     },
     ["xz"] = {
         ubuntu = {"apt", "xz-utils"},
@@ -86,7 +89,6 @@ pmwrapper = {
         windows = {"winget", "vim.vim"},
         ubuntu = {"apt", "vim"},
         archlinux = {"pacman", "vim"},
-        manjaro = {"pacman", "vim"},
     },
     ["visual-studio"] = { ref = "vs2022" },
     ["vs2022"] = {
@@ -94,17 +96,17 @@ pmwrapper = {
     },
     ["vscode"] = {
         windows = {"winget", "Microsoft.VisualStudioCode"},
-        archlinux = {"pacman", "code"},
-        manjaro = {"pacman", "code"},
+        archlinux = {"aur", "visual-studio-code-bin"},
+    },
+    ["vscodium"] = {
+        archlinux = {"aur", "vscodium-electron-bin"},
     },
     ["webkit2gtk"] = {
         ubuntu = {"apt", "libwebkit2gtk-4.0-37"},
         archlinux = {"pacman", "webkit2gtk"},
-        manjaro = {"pacman", "webkit2gtk"},
     },
     ["wechat"] = {
-        archlinux = {"aur", "wechat"},
-        manjaro = {"aur", "wechat"},
+        archlinux = {"aur", "wechat-universal-bwrap"},
     },
     ["wsl"] = {
         windows = {"winget", "Microsoft.WSL"},

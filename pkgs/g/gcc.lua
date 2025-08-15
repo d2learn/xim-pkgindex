1310af2e43161a52f7c1525b35e57bfa54c81886
sunrisepeak <speakshen@163.com>
2025-08-15 20:32:22 +0800

    add musl-gcc prebuild version & update gcc

--- pkgs/g/gcc.lua
+++ pkgs/g/gcc.lua
@@ -18,10 +18,12 @@ package = {
     keywords = {"compiler", "gnu", "gcc", "language", "c", "c++"},
 
     programs = {
+        "gcc-static", "g++-static",
         "gcc", "g++", "c++", "cpp",
         "addr2line", "ar", "as", "ld", "nm",
         "objcopy", "objdump", "ranlib", "readelf",
         "size", "strings", "strip",
+        "ldd", "loader", -- -> musl-ldd -> musl-lic.so
     },
 
     -- xvm: xlings version management
@@ -29,13 +31,13 @@ package = {
 
     xpm = {
         linux = {
-            -- toolchain build based on musl-gcc-static
+            --deps = { "musl-gcc" },
             ["latest"] = { ref = "15.1.0" },
-            ["15.1.0"] = "XLINGS_RES",
+            ["15.1.0"] = { }, -- deps musl-gcc
         },
         macosx = {
             ["latest"] = { ref = "15.1.0" },
-            ["15.1.0"] = "XLINGS_RES",
+            ["15.1.0"] = { }, --"XLINGS_RES",
         },
         windows = {
             ["latest"] = { ref = "15.1.0" },
@@ -44,8 +46,7 @@ package = {
     },
 }
 
-import("xim.xinstall")
-import("xim.xuninstall")
+import("xim.libxpkg.pkgmanager")
 import("xim.libxpkg.pkginfo")
 import("xim.libxpkg.xvm")
 import("xim.libxpkg.log")
@@ -56,13 +57,12 @@ local mingw_version_map = {
 
 function install()
     if is_host("windows") then
-        xinstall("mingw-w64@" .. mingw_version_map[pkginfo.version()])
-    else
-        local gccdir = pkginfo.install_file()
-            :replace(".tar.gz", "")
-            :replace(".zip", "")
-        os.tryrm(pkginfo.install_dir())
-        os.mv(gccdir, pkginfo.install_dir())
+        pkgmanager.install("mingw-w64@" .. mingw_version_map[pkginfo.version()])
+    elseif is_host("linux") then
+        pkgmanager.install("musl-gcc@" .. pkginfo.version())
+    elseif is_host("macosx") then
+        log.warn("TODO: macOS support for GCC is not implemented yet.")
+        return false
     end
     return true
 end
@@ -74,63 +74,14 @@ function config()
         log.warn("TODO: macOS support for GCC is not implemented yet.")
         return false
     elseif is_host("linux") then
-        local gcc_bindir = path.join(pkginfo.install_dir(), "bin")
-    
+
         log.info("add [ gcc, g++, c++, cpp, addr2line, ar, as, ld, nm, objcopy, objdump, ranlib, readelf, size, strings, strip ... ] commands")
         for _, prog in ipairs(package.programs) do
             xvm.add(prog, {
-                bindir = gcc_bindir,
-                alias = "x86_64-linux-musl-" .. prog,
+                version = "musl-gcc-" .. pkginfo.version(),
+                alias = "musl-" .. prog,
             })
         end
-
--- runtime lib
-        log.warn("add runtime libraries for musl-gcc-static...")
-        local musl_lib_dir = path.join(
-            pkginfo.install_dir(),
-            "x86_64-linux-musl", "lib"
-        )
-
-        -- add musl's libc libc.so
-        xvm.add("libc", {
-            version = "gcc-" .. pkginfo.version(),
-            filename = "libc.so",
-            bindir = musl_lib_dir,
-            type = "lib",
-            alias = "libc.so",
-        })
-
-        -- add ld.so (musl's ld.so wrapper)
-        xvm.add("ld-musl", {
-            version = "gcc-" .. pkginfo.version(),
-            filename = "ld-musl-x86_64.so.1",
-            bindir = musl_lib_dir,
-            type = "lib",
-            alias = "libc.so",
-        })
-
--- special commands
-        log.warn("add [ ldd ] (ld-musl-x86_64.so.1 --list)")
-        xvm.add("ldd", {
-            version = "gcc-" .. pkginfo.version(),
-            bindir = musl_lib_dir,
-            alias = "libc.so --list",
-            envs = {
-                -- ? alias = "libc.so --library-path musl_lib_dir --list",
-                LD_LIBRARY_PATH = musl_lib_dir,
-            }
-        })
-
-        log.warn("add [ musl-loader ] (ld-musl-x86_64.so.1)")
-        xvm.add("musl-loader", {
-            version = "gcc-" .. pkginfo.version(),
-            bindir = musl_lib_dir,
-            alias = "libc.so",
-            envs = {
-                -- ? alias = "libc.so --library-path musl_lib_dir",
-                LD_LIBRARY_PATH = musl_lib_dir,
-            }
-        })
 
         return true
     end
@@ -143,10 +94,6 @@ function uninstall()
         for _, prog in ipairs(package.programs) do
             xvm.remove(prog)
         end
-        xvm.remove("libc", "gcc-" .. pkginfo.version())
-        xvm.remove("ld-musl", "gcc-" .. pkginfo.version())
-        xvm.remove("ldd", "gcc-" .. pkginfo.version())
-        xvm.remove("musl-loader", "gcc-" .. pkginfo.version())
     end
     return true
 end
---
+++ pkgs/m/musl-gcc.lua
@@ -0,0 +1,129 @@
+package = {
+    -- base info
+    name = "musl-gcc",
+    description = "GCC, the GNU Compiler Collection ( prebuild with musl )",
+
+    authors = "GNU",
+    licenses = "GPL",
+    repo = "https://github.com/gcc-mirror/gcc",
+    docs = "https://gcc.gnu.org/wiki",
+
+    -- xim pkg info
+    type = "package",
+    archs = {"x86_64"},
+    status = "stable", -- dev, stable, deprecated
+    categories = {"compiler", "gnu", "language"},
+    keywords = {"compiler", "gnu", "gcc", "language", "c", "c++"},
+
+    programs = {
+        -- "musl-gcc-static", "musl-g++-static",
+        "musl-gcc", "musl-g++", "musl-c++", "musl-cpp",
+        "musl-addr2line", "musl-ar", "musl-as", "musl-ld", "musl-nm",
+        "musl-objcopy", "musl-objdump", "musl-ranlib", "musl-readelf",
+        "musl-size", "musl-strings", "musl-strip",
+    },
+
+    -- xvm: xlings version management
+    xvm_enable = true,
+
+    xpm = {
+        linux = {
+            -- toolchain build based on musl-gcc-static
+            ["latest"] = { ref = "15.1.0" },
+            ["15.1.0"] = {
+                -- TODO: create musl-gcc package xlings-res
+                url = "https://gitcode.com/xlings-res/gcc/releases/download/15.1.0/gcc-15.1.0-linux-x86_64.tar.gz",
+            },
+        },
+    },
+}
+
+import("xim.libxpkg.pkginfo")
+import("xim.libxpkg.xvm")
+import("xim.libxpkg.log")
+
+
+function install()
+    local gccdir = pkginfo.install_file()
+        :replace(".tar.gz", "")
+        :replace(".zip", "")
+    os.tryrm(pkginfo.install_dir())
+    os.mv(gccdir, pkginfo.install_dir())
+    return true
+end
+
+function config()
+    local gcc_bindir = path.join(pkginfo.install_dir(), "bin")
+
+    for _, prog in ipairs(package.programs) do
+        xvm.add(prog, {
+            bindir = gcc_bindir,
+            alias = "x86_64-linux-" .. prog,
+        })
+    end
+
+-- runtime lib
+    log.warn("add runtime libraries for musl-gcc-static...")
+    local musl_lib_dir = path.join(
+        pkginfo.install_dir(),
+        "x86_64-linux-musl", "lib"
+    )
+
+    -- add musl's libc libc.so
+    xvm.add("musl-libc", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "libc.so",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libc.so",
+    })
+
+    -- add ld.so (musl's ld.so wrapper)
+    xvm.add("ld-musl",
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "ld-musl-x86_64.so.1",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libc.so",
+    })
+
+-- special commands
+    xvm.add("musl-ldd", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        bindir = musl_lib_dir,
+        alias = "libc.so --list",
+        envs = {
+            -- ? alias = "libc.so --library-path musl_lib_dir --list",
+            LD_LIBRARY_PATH = musl_lib_dir,
+        }
+    })
+
+    xvm.add("musl-loader", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        bindir = musl_lib_dir,
+        alias = "libc.so",
+        envs = {
+            -- ? alias = "libc.so --library-path musl_lib_dir",
+            LD_LIBRARY_PATH = musl_lib_dir,
+        }
+    })
+
+    log.info("add static wrapper for musl-gcc ...")
+    xvm.add("musl-gcc-static", { alias = "musl-gcc -static" })
+    xvm.add("musl-g++-static", { alias = "musl-g++ -static" })
+
+    return true
+end
+
+function uninstall()
+    for _, prog in ipairs(package.programs) do
+        xvm.remove(prog)
+    end
+    xvm.remove("musl-libc", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("ld-musl", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("musl-ldd", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("musl-loader", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("musl-gcc-static")
+    xvm.remove("musl-g++-static")
+    return true
+end

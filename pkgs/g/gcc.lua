dfdce406382d563a1cc4aeeaad0a84e2feb7ba12
sunrisepeak <speakshen@163.com>
2025-08-15 15:14:28 +0800

    gcc: use musl-gcc-static to fix dependencies issues for multi-platform

--- pkgs/g/gcc.lua
+++ pkgs/g/gcc.lua
@@ -17,17 +17,21 @@ package = {
     categories = {"compiler", "gnu", "language"},
     keywords = {"compiler", "gnu", "gcc", "language", "c", "c++"},
 
+    programs = {
+        "gcc", "g++", "c++", "cpp",
+        "addr2line", "ar", "as", "ld", "nm",
+        "objcopy", "objdump", "ranlib", "readelf",
+        "size", "strings", "strip",
+    },
+
     -- xvm: xlings version management
     xvm_enable = true,
 
     xpm = {
         linux = {
+            -- toolchain build based on musl-gcc-static
             ["latest"] = { ref = "15.1.0" },
             ["15.1.0"] = "XLINGS_RES",
-            ["14.2.0"] = { url = __gcc_url("14.2.0") },
-            ["13.3.0"] = { url = __gcc_url("13.3.0") },
-            ["12.4.0"] = { url = __gcc_url("12.4.0") },
-            ["11.5.0"] = { url = __gcc_url("11.5.0") },
         },
         macosx = {
             ["latest"] = { ref = "15.1.0" },
@@ -44,6 +48,7 @@ import("xim.xinstall")
 import("xim.xuninstall")
 import("xim.libxpkg.pkginfo")
 import("xim.libxpkg.xvm")
+import("xim.libxpkg.log")
 
 local mingw_version_map = {
     ["15.1.0"] = "13.0.0",
@@ -63,35 +68,85 @@ function install()
 end
 
 function config()
-    if is_host("windows") then return true end
-
-    local gcc_bindir = path.join(pkginfo.install_dir(), "bin")
-    local ld_lib_path = string.format("%s:%s", path.join(pkginfo.install_dir(), "lib64"), os.getenv("LD_LIBRARY_PATH") or "")
+    if is_host("windows") then
+        return true
+    elseif is_host("macosx") then
+        log.warn("TODO: macOS support for GCC is not implemented yet.")
+        return false
+    elseif is_host("linux") then
+        local gcc_bindir = path.join(pkginfo.install_dir(), "bin")
     
-    local config = {
-        bindir = gcc_bindir,
-        envs = {
-            ["LD_LIBRARY_PATH"] = ld_lib_path,
-        }
-    }
+        log.info("add [ gcc, g++, c++, cpp, addr2line, ar, as, ld, nm, objcopy, objdump, ranlib, readelf, size, strings, strip ... ] commands")
+        for _, prog in ipairs(package.programs) do
+            xvm.add(prog, {
+                bindir = gcc_bindir,
+                alias = "x86_64-linux-musl-" .. prog,
+            })
+        end
 
-    xvm.add("gcc", config)
-    if is_host("macosx") then config.alias = "gcc-15" end
-    xvm.add("g++", config)
-    if is_host("macosx") then config.alias = "g++-15" end
-    xvm.add("c++", config)
-    if is_host("macosx") then config.alias = "c++-15" end
+-- runtime lib
+        log.warn("add runtime libraries for musl-gcc-static...")
+        local musl_lib_dir = path.join(
+            pkginfo.install_dir(),
+            "x86_64-linux-musl", "lib"
+        )
 
-    return true
+        -- add musl's libc libc.so
+        xvm.add("libc", {
+            version = "gcc-" .. pkginfo.version(),
+            filename = "libc.so",
+            bindir = musl_lib_dir,
+            type = "lib",
+            alias = "libc.so",
+        })
+
+        -- add ld.so (musl's ld.so wrapper)
+        xvm.add("ld-musl", {
+            version = "gcc-" .. pkginfo.version(),
+            filename = "ld-musl-x86_64.so.1",
+            bindir = musl_lib_dir,
+            type = "lib",
+            alias = "libc.so",
+        })
+
+-- special commands
+        log.warn("add [ ldd ] (ld-musl-x86_64.so.1 --list)")
+        xvm.add("ldd", {
+            version = "gcc-" .. pkginfo.version(),
+            bindir = musl_lib_dir,
+            alias = "libc.so --list",
+            envs = {
+                -- ? alias = "libc.so --library-path musl_lib_dir --list",
+                LD_LIBRARY_PATH = musl_lib_dir,
+            }
+        })
+
+        log.warn("add [ musl-loader ] (ld-musl-x86_64.so.1)")
+        xvm.add("musl-loader", {
+            version = "gcc-" .. pkginfo.version(),
+            bindir = musl_lib_dir,
+            alias = "libc.so",
+            envs = {
+                -- ? alias = "libc.so --library-path musl_lib_dir",
+                LD_LIBRARY_PATH = musl_lib_dir,
+            }
+        })
+
+        return true
+    end
 end
 
 function uninstall()
     if is_host("windows") then
         xuninstall("mingw-w64@" .. mingw_version_map[pkginfo.version()])
-    else
-        xvm.remove("gcc")
-        xvm.remove("g++")
-        xvm.remove("c++")
+    elseif is_host("linux") then
+        for _, prog in ipairs(package.programs) do
+            xvm.remove(prog)
+        end
+        xvm.remove("libc", "gcc-" .. pkginfo.version())
+        xvm.remove("ld-musl", "gcc-" .. pkginfo.version())
+        xvm.remove("ldd", "gcc-" .. pkginfo.version())
+        xvm.remove("musl-loader", "gcc-" .. pkginfo.version())
     end
     return true
 end

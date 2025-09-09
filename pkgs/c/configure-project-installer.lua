6e511984db63c342f99c38aacaf41109302aea76
sunrisepeak <speakshen@163.com>
2025-09-09 17:48:24 +0800

    add configure-project-installer

---
+++ pkgs/c/configure-project-installer.lua
@@ -0,0 +1,77 @@
+package = {
+    name = "configure-project-installer",
+    description = "Build & Install (from source) helper tools for ./configure-based projects",
+    authors = "sunrisepeak",
+    maintainers = "d2learn",
+    licenses = "Apache-2.0",
+    repo = "https://github.com/d2learn/xim-pkgindex",
+
+    type = "script",
+    status = "stable",
+    categories = {"tools", "build", "helper"},
+    keywords = {"configure", "make", "build", "install"},
+
+    programs = { "configure-project-installer" },
+
+    xpm = {
+        linux = {
+            deps = { "make", "gcc" },
+            ["latest"] = { ref = "0.0.1" },
+            ["0.0.1"] = {}
+        },
+        macosx = { ref = "linux" },
+    },
+}
+
+import("xim.libxpkg.log")
+import("xim.libxpkg.utils")
+import("xim.libxpkg.system")
+
+local __xscript_input = {
+    ["--args"] = false,
+    ["--project-dir"] = false,
+    ["--project-url"] = false, -- TODO: support git clone from url
+    ["--force"] = false,
+}
+
+function xpkg_main(installdir, ...)
+
+    installdir = installdir or "configure-project-installer"
+    installdir = path.absolute(installdir)
+
+    local _, cmds = utils.input_args_process(__xscript_input, {...})
+    local configure_args = cmds["--args"] or ""
+    local srcdir = cmds["--project-dir"] or "."
+    local abs_srcdir = path.absolute(srcdir)
+
+    configure_args = string.format("%s --prefix=%s", configure_args, installdir)
+
+    cprint("")
+    cprint("\t ${bright}Configure Project Installer - 0.0.1${clear}")
+    cprint("")
+    cprint("project-dir: ${dim green}" .. abs_srcdir)
+    cprint("install-dir: ${dim green}" .. installdir)
+    cprint("args:   ${dim green}" .. configure_args)
+    cprint("")
+
+    os.cd(abs_srcdir)
+
+    if not os.isfile("configure") then
+        log.error("missing configure script in project directory")
+        log.error("${red}error: missing or invalid project directory: ${clear}" .. abs_srcdir)
+        cprint("")
+        cprint("Usage: ${dim cyan}configure-project-installer <install-dir> [ --project-dir xx ] [--args xx] [--force]")
+        cprint("")
+        return
+    end
+
+    os.sleep(2000) -- wait for 2 seconds to let user cancel if needed
+
+    -- run ./configure
+    local configure_cmd = string.format("./configure %s", configure_args)
+    system.exec(configure_cmd)
+    system.exec("make -j20", { retry = 3 })
+    system.exec("make install")
+
+    log.info("✅ installed to: ${green}%s", installdir)
+end

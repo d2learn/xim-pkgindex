6532873aedcb1bf7afe75d65fb420ef5ac5fdde9
sunrisepeak <speakshen@163.com>
2025-09-10 20:07:17 +0800

    add awesome package index repository

--- xim-indexrepos.lua
+++ xim-indexrepos.lua
@@ -4,6 +4,10 @@ xim_indexrepos = {
         ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-template.git",
         ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-template.git",
     },
+    ["awesome"] = {
+        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-awesome.git",
+        ["CN"] = "https://github.com/d2learn/xim-pkgindex-awesome.git",
+    },
     ["d2x"] = {
         ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-d2x.git",
         ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-d2x.git",

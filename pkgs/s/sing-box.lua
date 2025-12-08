a21e34384594db32731e65f4d9b1e6f0c3301bb4
sunrisepeak <speakshen@163.com>
2025-12-09 01:38:59 +0800

    add sing-box and sing-box helper tools

---
+++ pkgs/s/sing-box.lua
@@ -0,0 +1,59 @@
+package = {
+    -- base info
+    name = "sing-box",
+    description = "The universal proxy platform",
+    homepage = "https://sing-box.sagernet.org/",
+    
+    maintainers = "SagerNet",
+    licenses = "GPL-3.0-or-later",
+    repo = "https://github.com/SagerNet/sing-box",
+    docs = "https://sing-box.sagernet.org/",
+
+    -- xim pkg info
+    archs = {"x86_64", "aarch64", "arm", "armv7h"},
+    status = "stable",
+    categories = {"proxy", "network"},
+    keywords = {"proxy", "vpn", "shadowsocks", "vmess", "trojan", "vless"},
+
+    -- xvm: xlings version management
+    xvm_enable = true,
+
+    xpm = {
+        debain = {
+            ["latest"] = { ref = "1.12.12" },
+            ["1.12.12"] = {
+                url = "https://github.com/SagerNet/sing-box/releases/download/v1.12.12/sing-box-1.12.12-linux-amd64.tar.gz",
+                sha256 = nil
+            },
+        },
+        ubuntu = { ref = "debain" },
+    },
+}
+
+import("xim.libxpkg.pkginfo")
+import("xim.libxpkg.xvm")
+
+function install()
+    -- install prebuild binary
+    print("Installing sing-box from prebuild...")
+    
+    -- Create installation directory
+    os.tryrm(pkginfo.install_dir())
+
+    local singbox_dir = pkginfo.install_file():replace(".tar.gz", "")
+    os.mv(singbox_dir, pkginfo.install_dir())
+
+    return true
+end
+
+function config()
+    -- config xvm
+    xvm.add("sing-box", pkginfo.version())
+    
+    return true
+end
+
+function uninstall()
+    xvm.remove("sing-box")
+    return true
+end

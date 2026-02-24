eb766f7e68baac3a7f75bfd6539ebeeeef2a7603
sunrisepeak <speakshen@163.com>
2026-02-25 05:00:54 +0800

    gcc: update

--- pkgs/g/gcc.lua
+++ pkgs/g/gcc.lua
@@ -52,6 +52,7 @@ import("xim.libxpkg.pkginfo")
 import("xim.libxpkg.log")
 import("xim.libxpkg.xvm")
 import("xim.libxpkg.pkgmanager")
+import("xim.libxpkg.elfpatch")
 
 local gcc_tool = {
     ["gcc-ar"] = true, ["gcc-nm"] = true, ["gcc-ranlib"] = true,
@@ -71,6 +72,10 @@ local version_map_gcc2mingw = {
     ["15.1.0"] = "13.0.0",
 }
 
+local gcc_libexec_tools = {
+    "cc1", "cc1plus", "collect2", "lto-wrapper", "lto1", "f951"
+}
+
 function install()
     if is_host("windows") then
         pkgmanager.install("mingw-w64@" .. version_map_gcc2mingw[pkginfo.version()])
@@ -81,6 +86,12 @@ function install()
             symlink = true,
             verbose = true,
         })
+
+        -- Default manual mode: package author controls loader/rpath patching.
+        elfpatch.auto(false)
+
+        local rpath_list = __manual_rpath_list()
+        __patch_gcc_binaries(rpath_list)
     end
     return true
 end
@@ -169,4 +180,51 @@ function __config_linux()
     })
 
     return true
+end
+
+function __append_if_libdir_exists(values, root_dir)
+    if not root_dir then
+        return
+    end
+    for _, sub in ipairs({"lib64", "lib"}) do
+        local d = path.join(root_dir, sub)
+        if os.isdir(d) then
+            table.insert(values, d)
+            return
+        end
+    end
+end
+
+function __manual_rpath_list()
+    local values = {}
+    __append_if_libdir_exists(values, pkginfo.install_dir())
+    __append_if_libdir_exists(values, pkginfo.install_dir("glibc", "2.39"))
+    __append_if_libdir_exists(values, pkginfo.install_dir("binutils", "2.42"))
+    return values
+end
+
+function __patch_one_elf(filepath, rpath_list)
+    if not os.isfile(filepath) then
+        return
+    end
+    elfpatch.patch_elf_loader_rpath(filepath, {
+        loader = "system",
+        rpath = rpath_list,
+        strict = false
+    })
+end
+
+function __patch_gcc_binaries(rpath_list)
+    local bindir = path.join(pkginfo.install_dir(), "bin")
+    for _, prog in ipairs(package.programs) do
+        __patch_one_elf(path.join(bindir, prog), rpath_list)
+    end
+
+    local libexec_dir = path.join(
+        pkginfo.install_dir(),
+        "libexec", "gcc", "x86_64-linux-gnu", pkginfo.version()
+    )
+    for _, tool in ipairs(gcc_libexec_tools) do
+        __patch_one_elf(path.join(libexec_dir, tool), rpath_list)
+    end
 end

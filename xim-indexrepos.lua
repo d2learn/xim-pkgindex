9a2ec57668e3fb3d21a6207fe28c430076d0bbd3
sunrisepeak <speakshen@163.com>
2026-02-24 21:50:31 +0800

    xim: install/config split and multi-repo simplification
    - T15: Add config() hooks to rustup.lua and musl-cross-make.lua,
      moving xvm.add() out of install() for proper multi-subos support
    - T22: Simplify xim-indexrepos to only retain awesome as the default
      extension entry point; other repos are added on demand via
      index-repo packages

--- xim-indexrepos.lua
+++ xim-indexrepos.lua
@@ -1,23 +1,7 @@
 xim_indexrepos = {
-    -- ["namespace"] = "url",
-    ["xim"] = {
-        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-template.git",
-        ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-template.git",
-    },
+    -- keep awesome as default extension entry; other repos are added on demand
     ["awesome"] = {
         ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-awesome.git",
         ["CN"] = "https://github.com/d2learn/xim-pkgindex-awesome.git",
-    },
-    ["d2x"] = {
-        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-d2x.git",
-        ["CN"] = "https://gitee.com/xlings-res/xim-pkgindex-d2x.git",
-    },
-    ["fromsource"] = {
-        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-fromsource.git",
-        ["CN"] = "https://github.com/d2learn/xim-pkgindex-fromsource.git",
-    },
-    ["scode"] = {
-        ["GLOBAL"] = "https://github.com/d2learn/xim-pkgindex-scode.git",
-        ["CN"] = "https://github.com/d2learn/xim-pkgindex-scode.git",
     },
 }

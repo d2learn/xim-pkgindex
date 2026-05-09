596a485d59980ed4956e9bc76d444628e8ff0858
SPeak <speakshen@163.com>
2026-05-10 02:39:44 +0800

    feat(mcpp): add 0.0.3 (#154)

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -42,7 +42,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.2" },
+            ["latest"] = { ref = "0.0.3" },
+            ["0.0.3"] = "XLINGS_RES",
             ["0.0.2"] = "XLINGS_RES",
             ["0.0.1"] = "XLINGS_RES",
         },
